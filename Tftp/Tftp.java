29de7823a815acdcd3d227a8e082006398ea0525
Esteves, Kalei (Student) <ke131@students.waikato.ac.nz>
2024-09-18 23:04:38 +1200

    Fixed indexing, formatting, and added documentation

--- Tftp/Tftp.java
+++ Tftp/Tftp.java
@@ -7,29 +7,60 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
+/**
+ * Tftp class.
+ *
+ * Provides constants for the TftpServer and TftpClient program.
+ *
+ * This class has public static methods to support operations for Trivial File
+ * Transfer between a server and client.
+ *
+ * The protocol is based on a simplified version of TFTP RFC 1350.
+ *
+ * @see     Tftp
+ * @see     TftpServer
+ * @see     TftpClient
+ */
 public class Tftp {
 
     public static final String SRC_DIR = "server";
     public static final String OUT_DIR = "client";
+    public static final String ENCODING = "ASCII";
 
-    public static final byte RRQ = 1;
-    public static final byte DATA = 2;
-    public static final byte ACK = 3;
-    public static final byte ERROR = 4;
+    public static final byte RRQ = 1;           /* Request packet */
+    public static final byte DATA = 2;          /* File data packet */
+    public static final byte ACK = 3;           /* Acknowledge packet */
+    public static final byte ERROR = 4;         /* Error message packet */
 
-    public static final int LIMIT = 131072;
-    public static final int BUFFER = 514;
-    public static final int OFFSET = 1;
-    public static final int HEADER_SIZE = OFFSET + OFFSET;
-    public static final int BLOCK_SIZE = BUFFER - HEADER_SIZE;
-   
-    public static final int PORT = 69;
-    public static final int MAX_ATTEMPTS = 5;
-    public static final int TIMEOUT = 20000;
-    public static final int WAIT = 1000;
-    public static final int PAUSE = 50;
+    public static final int BUFFER = 512;       /* Standard TFTP Buffer */
+    public static final int LIMIT = 131072;     /* Limit for 1-byte headers */
+    public static final int OFFSET = 1;         /* Offset for 1-byte headers */
 
-   	public static Path setLocalPath(String directory) throws IOException {
+    public static final int HEADER = OFFSET * 2;
+    public static final int BLOCK = BUFFER - HEADER;
+
+    public static final int PORT = 69;          /* Default listening port */
+    public static final int ATTEMPTS = 5;       /* Max tries to send a block */
+    public static final int TIMEOUT = 20000;    /* Timeout socket connection */
+    public static final int WAIT = 1000;        /* Timeout for response wait */
+    public static final int PAUSE = 50;         /* Power-nap for performance */
+
+    /**
+     * Tftp private constructor.
+     * This prevents class Tftp from being instantiated.
+     */
+    private Tftp(){}
+
+
+   	/**
+     * Sets the path of the host's local directory.
+     *
+     * @param   directory   The directory to resolve, the folder name.
+     * @return  The local path that exists, or newly created.
+     * @throws  IOException
+     */
+    public static Path setLocalPath(String directory) throws IOException {
 
         Path localPath = Paths.get(directory);
         if (Files.exists(localPath)) {
@@ -42,24 +73,92 @@ public class Tftp {
                 throw new IOException("Could not resolve a local directory.");
             }
         }
-    } 
-
-    public static int getTotalBlocks(int fileSize) {
-
-    	return (fileSize + BLOCK_SIZE - 1) / BLOCK_SIZE;
     }
 
+
+    /**
+     * Gets the Path of file that was requested.
+     *
+     * @param   directory   The directory the file is located in.
+     * @param   fileName    The file name to find in the folder.
+     * @return  The Path object that represents the file's path.
+     * @throws  IOException
+     */
+    public static Path getFilePath(Path directory, String fileName) throws IOException {
+
+        Path filePath = directory.resolve(fileName);
+        if (Files.exists(filePath)) {
+            return filePath;
+        } else {
+            throw new IOException("Could not resolve the file path.");
+        }
+    }
+
+
+    /**
+     * Gets the bytes of file that was requested.
+     *
+     * @param   filePath   The Path that describes where the file is located.
+     * @return  The array of bytes containing the file, if under the LIMIT.
+     * @throws  IOException
+     */
+    public static byte[] getFileBytes(Path filePath) throws IOException {
+
+        byte[] fileBytes = Files.readAllBytes(filePath);
+        int size = fileBytes.length;
+        if (size < LIMIT) {
+            return fileBytes;
+        } else {
+            throw new IOException("Size exceeded limit " + size + " > " + LIMIT);
+        }
+    }
+
+
+    /**
+     * Calculates total blocks for a file.
+     *
+     * @see     #BLOCK
+     * @param   fileSize    The int size, the length of file bytes.
+     * @return  The local path that exists, or newly created.
+     */
+    public static int getTotalBlocks(int fileSize) {
+
+        return (fileSize + BLOCK - 1) / BLOCK;
+    }
+
+
+    /**
+     * Creates an RRQ packet for the file name requested.
+     *
+     * @param   fileName    The String file name to request.
+     * @param   addr        The InetAddress of the TftpServer.
+     * @return  The DatagramPacket format for an RRQ.
+     * @throws  IOException
+     */
     public static DatagramPacket rrqPacket(String fileName, InetAddress addr) throws IOException {
 
         byte[] msg = fileName.getBytes();
         byte[] pkt = new byte[msg.length + 1];
 
         pkt[0] = RRQ;
-        System.arraycopy(msg, 0, pkt, 1, msg.length);
+        System.arraycopy(msg, 0, pkt, OFFSET, msg.length);
 
         return new DatagramPacket(pkt, pkt.length, addr, PORT);
     }
 
+
+    /**
+     * Creates a DATA packet array for the file to be sent as.
+     *
+     * For the total amount of blocks required, creates a DatagramPacket for
+     * each one that contains its block number and partition of file bytes.
+     *
+     * @param   file    The file byte array to divide and package.
+     * @param   addr    The InetAddress of the TftpServer.
+     * @param   port    The int port that the packets will be sent over.
+     * @return  The array of formatted DatagramPackets containing file blocks.
+     * @throws  IOException
+     */
     public static DatagramPacket[] dataPacket(byte[] file, InetAddress addr, int port) throws IOException {
 
         int sent = 0;
@@ -73,13 +172,13 @@ public class Tftp {
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
 
             int remaining = size - sent;
-            int toSend = Math.min(remaining, BLOCK_SIZE);
+            int toSend = Math.min(remaining, BLOCK);
 
-            bos.write(DATA);
-            bos.write((byte)(block + 1));
-            bos.write(file, sent, toSend);
+            bos.write(DATA);                /* Writing DATA Op Code */
+            bos.write((byte)(block + 1));   /* Writing block number */
+            bos.write(file, sent, toSend);  /* Writing part of file bytes */
 
-            byte[] pkt = bos.toByteArray();
+            byte[] pkt = bos.toByteArray(); /* Get the new array from stream */
             packets[block] = new DatagramPacket(pkt, pkt.length, addr, port);
 
             sent += toSend;
@@ -87,23 +186,52 @@ public class Tftp {
         return packets;
     }
 
+
+    /**
+     * Creates an ACK packet with the int block number to acknowledge.
+     * Note: The block number is cast as a byte.
+     *
+     * @param   block   The int block number to acknowledge.
+     * @param   addr    The InetAddress of the destination.
+     * @param   port    The port to send the packet through.
+     * @return  The DatagramPacket format for an ACK packet with a block number.
+     * @throws  IOException
+     */
     public static DatagramPacket ackPacket(int block, InetAddress addr, int port) throws IOException {
 
         byte[] pkt = {ACK, (byte)block};
         return new DatagramPacket(pkt, pkt.length, addr, port);
     }
 
+
+    /**
+     * Creates an ERROR packet with the String message to send.
+     *
+     * @param   message The String error message to send with the packet.
+     * @param   addr    The InetAddress of the destination.
+     * @param   port    The port to send the packet through.
+     * @return  The DatagramPacket format for an ERROR packet with a message.
+     * @throws  IOException
+     */
     public static DatagramPacket errorPacket(String message, InetAddress addr, int port) throws IOException {
 
         byte[] msg = message.getBytes();
-        byte[] pkt = new byte[msg.length + 1];
+        byte[] pkt = new byte[msg.length + OFFSET];
 
         pkt[0] = ERROR;
-        System.arraycopy(msg, 0, pkt, 1, msg.length);
+        System.arraycopy(msg, 0, pkt, OFFSET, msg.length);
 
         return new DatagramPacket(pkt, pkt.length, addr, port);
     }
 
+
+    /**
+     * Gets the String message of a 1-byte RRQ or ERROR packet.
+     *
+     * @param   data    The byte array of data from the DatagramPacket.
+     * @return  The String message, extracted from the packet's array.
+     * @throws  IOException
+     */
     public static String getString(byte[] data) throws IOException {
 
     	int len = data.length - OFFSET;
@@ -113,7 +241,7 @@ public class Tftp {
     		msg[i] = data[i + OFFSET];
     	}
 
-        String message = new String(msg);
+        String message = new String(msg, ENCODING);
     	return message.trim();
     }
 }
--- Tftp/TftpServer.java
+++ Tftp/TftpServer.java
@@ -7,39 +7,66 @@ import java.nio.file.Files;
 import java.io.IOException;
 
 
+/**
+ * TftpServer class.
+ *
+ * This class extends Thread.
+ * Describes the server-side behaviour of Trival File Transfer Protocol.
+ *
+ * The protocol is based on a simplified version of TFTP RFC 1350.
+ * The rules are defined in {@link Tftp} by public constants and static methods.
+ *
+ * @see 	Tftp
+ * @see 	TftpClient
+ */
 public class TftpServer extends Thread {
 
-    private final InetAddress address;
-    private final Path directory;
+    private final InetAddress addr;
+    private final Path path;
 
-    public TftpServer(InetAddress address) throws IOException {
+    /**
+     * TftpServer constructor.
+     * Resolves this host's local directory.
+     *
+     * @param 	addr  The address of this TftpServer.
+     * @throws 	IOException
+     */
+    public TftpServer(InetAddress addr) throws IOException {
 
-		this.address = address;
-		this.directory = Tftp.setLocalPath(Tftp.SRC_DIR);
+		this.addr = addr;
+		this.path = Tftp.setLocalPath(Tftp.SRC_DIR);
 	}
 
+
+	/**
+	 * Entry point main.
+	 *
+	 * Starts the main Thread of TftpServer. Creates a new instance, and passes
+	 * it the InetAddress of TftpServer.
+	 *
+	 * @param   args  	The arguments should be empty.
+	 */
 	public static void main(String[] args){
 
 		if (args.length == 0) {
-
 			System.out.println("\nWelcome to local TftpServer!\n");
 
 			try {
-				InetAddress address = InetAddress.getLocalHost();
-				TftpServer server = new TftpServer(address);
+				InetAddress addr = InetAddress.getLocalHost();
+				TftpServer server = new TftpServer(addr);
 
 				System.out.println("For the remote client, enter something like:");
-				System.out.println("$ java TftpClient " + address.getHostAddress() + " cat.txt");
+				System.out.println("$ java TftpClient " + addr.getHostAddress() + " cat.txt");
 
 				server.start();
-				server.join();
+				server.join(); /* Waits for the process to be complete */
 
 			} catch (InterruptedException e) {
 				System.out.println("InterruptedException: " + e.getMessage());
 			} catch (IOException e) {
 				System.out.println("IOException: " + e.getMessage());
 			} finally {
-				System.out.println("Closing TftpServer...");
+				System.out.println("Closing TftpServer...\n");
 			}
 		} else {
 			System.out.println("Too many arguments...");
@@ -47,6 +74,18 @@ public class TftpServer extends Thread {
 		}
 	}
 
+
+	/**
+	 * Runs the TftpServer process on start.
+	 *
+	 * Opens a DatagramSocket in the try-with-resources block, then waits,
+	 * until {@link Tftp#TIMEOUT} for a TftpClient's request.
+	 *
+	 * Handles the request by extracting the data from the RRQ DatagramPacket,
+	 * then calls the supporting methods to resolve and package the file.
+	 *
+	 * @see 	#transfer(DatagramSocket socket, byte[] file, InetAddress addr, int port)
+	 */
 	@Override
 	public void run() {
 
@@ -54,111 +93,136 @@ public class TftpServer extends Thread {
 			DatagramSocket client = new DatagramSocket(Tftp.PORT);
 		) {
 			System.out.println("\nWaiting on port " + Tftp.PORT + "...\n");
-           	client.setSoTimeout(Tftp.TIMEOUT);
-
            	DatagramPacket pkt = new DatagramPacket(new byte[Tftp.BUFFER], Tftp.BUFFER);
+
+           	client.setSoTimeout(Tftp.TIMEOUT);
            	client.receive(pkt);
 
            	System.out.println("\nRequest received!\n");
-            byte[] rrq = pkt.getData();
-            byte type = rrq[0];
+            byte[] rrq = pkt.getData();  	/* The data from the request */
+            byte type = rrq[0];             /* The byte Op Code to check */
 
-            InetAddress address = pkt.getAddress();
+            InetAddress addr = pkt.getAddress();
             int port = pkt.getPort();
 
             try {
+            	/* Checks that the first packet is an RRQ */
 	            if (type == Tftp.RRQ) {
 	            	String fileName = Tftp.getString(rrq);
 
+	            	/* Checks that the file name exists in the request */
 	            	if (fileName != null) {
-	            		Path filePath = getFilePath(directory, fileName);
+	            		Path filePath = Tftp.getFilePath(path, fileName);
 
+	            		/* Checks that the file exists in the path */
 	            		if (filePath != null) {
-	            			byte[] file = getFileBytes(filePath);
 
-	            			System.out.println("\tFile: " + fileName);
-	            			System.out.println("\tPath: " + filePath);
-	            			System.out.println("\tSize: " + file.length);
+	            			/* Gets the byte array of the file */
+	            			byte[] file = Tftp.getFileBytes(filePath);
 
-	            			if (transfer(client, file, address, port)) {
+	            			System.out.println("\tFile:\t" + fileName);
+	            			System.out.println("\tPath:\t" + filePath);
+	            			System.out.println("\tSize:\t" + file.length);
+
+	            			/* Transfer file and checks if successful */
+	            			if (transfer(client, file, addr, port)) {
 	            				System.out.println("\nFile transfer was successful!\n");
 	            			} else {
-	            				throw new IOException("Could not transfer the file.");
+	            				throw new IOException("Error while transferring packets.");
 	            			}
 	            		} else {
-	            			throw new IOException("Could not find the file.");
+	            			throw new IOException("File was not found at specified path.");
 	            		}
 	            	} else {
-	            		throw new IOException("Could not read the file name.");
+	            		throw new IOException("File name was not found in request.");
 	            	}
 	            } else {
-	            	throw new IOException("Did not receive and RRQ packet.");
+	            	throw new IOException("Packet received was not of type RRQ.");
 	          }
             } catch (IOException e) {
-            	System.out.println("Error: " + e.getMessage());
-            	client.send(Tftp.errorPacket(e.getMessage(), address, port));
-            }      
+            	String msg = e.getMessage(); 	/* Gets error message to send out */
+            	client.send(Tftp.errorPacket(msg, addr, port)); /* To TftpClient */
+            	System.out.println("Transfer failed: " + msg);	/* To TftpServer */
+            }
 		} catch (Exception e) {
          	System.out.println("Fatal Error: " + e.getMessage());
          } finally {
-         	System.out.println("Closing TftpClient Socket...");
+         	System.out.println("\nClosing TftpClient Socket...");
          }
 	}
 
-	private boolean transfer(DatagramSocket socket, byte[] file, InetAddress address, int port) throws IOException {
 
-        DatagramPacket ack = new DatagramPacket(new byte[Tftp.HEADER_SIZE], Tftp.HEADER_SIZE);
-        DatagramPacket[] packets = Tftp.dataPacket(file, address, port);
+	/**
+	 * Transfers the data to the TftpClient.
+	 *
+	 * Takes the file bytes from the local TftpServer directory, then calls
+	 * the method to package the data into blocks with headers. Once packaged,
+	 * then sends over a block at a time by waiting for the TftpClient to send
+	 * an ACK back for the next.
+	 *
+	 * Will try to resend one packet for a maximum of attemps set by the value
+	 * of {@link Tftp#ATTEMPTS} and returns false if reached.
+	 *
+	 * @see 	#dataPacket(byte[] file, InetAddress addr, int port)
+	 *
+	 * @param   socket  The DatagramSocket the TftpClient is connected to.
+	 * @param   file    The byte array containing the requested file to send.
+	 * @param   addr    The InetAddress of the destination.
+	 * @param   port    The int port number to send the packets through.
+	 * @return  True if the file transfer was successful, False otherwise.
+	 * @throws  IOException
+	 */
+	private boolean transfer(DatagramSocket socket, byte[] file, InetAddress addr, int port) throws IOException {
+
+        DatagramPacket ack = new DatagramPacket(new byte[Tftp.HEADER], Tftp.HEADER);
+        DatagramPacket[] packets = Tftp.dataPacket(file, addr, port);
 
         int total = packets.length;
-        int attempts = 0;
-        int block = 0;
+        int attempts = 0;	/* Failed attempts made to send a block */
+        int block = 0; 		/* The amount of blocks sent, and the index */
 
-        System.out.println("\nTransferring " + total + " packets...\n");      		
+        System.out.println("\nTransferring " + total + " packets...\n");
 
-        while (attempts < Tftp.MAX_ATTEMPTS) {
+        /**
+         * The following loop will send a packet block on each iteration.
+         *
+         * This loop will break when TftpServer fails to send a packet too many
+         * times, or if any exception is thrown throughout the process.
+         */
+        while (attempts < Tftp.ATTEMPTS) {
 
-		   	socket.send(packets[block]);
+		   	socket.send(packets[block]);	/* Sends block to TftpClient */
 
-        	socket.setSoTimeout(Tftp.WAIT);
+        	socket.setSoTimeout(Tftp.WAIT); /* Short wait to receive an ACK */
     		socket.receive(ack);
 
         	byte[] arr = ack.getData();
-        	int sent = arr[1] & 0xFF;  
-        	int next = block + 1;
+        	int toSend = arr[1] & 0xFF;   	/* Gets the int of ACK'd block */
+        	int next = block + 1;    		/* Calculates the expected block */
 
-        	if (sent == total) {
-        		socket.send(Tftp.ackPacket(0, address, port)); 
-        		return true;
-        	} else if (sent == next) {  
-        		System.out.print("\t\rSending block " + next + "...\r");      			        			        		
-        		block = sent;     		 				      		
+        	/*
+        	 * Checks the expected block number against actual values.
+        	 *
+        	 * Note: 	The packet's array is 0-indexed, so any attempt to
+        	 * 			access it must be done with a value less than the block
+        	 * 			number requested.
+        	 *
+        	 * 			For example, the value of toSend is the 'next' block
+        	 * 			requested by TftpClient, but will also be the index for
+        	 * 			that block in this packet array.
+        	 */
+        	if (next == total) {
+        		System.out.print("\t\rSent total of " + total + " packets!\r");
+        		socket.send(Tftp.ackPacket(0, addr, port));
+       			return true;
+        	} else if (next == toSend) {
+        		System.out.print("\t\rSent " + next + "/" + total + " packets\r");
+        		block = toSend;  /* To send the next block over */
         	} else {
-        		System.out.print("\n\t\rRetrying block " + sent + "...\r");
-        		attempts++;	    		
+        		System.out.print("\n\t\rRetrying packet " + block + "...\r");
+        		attempts++;
         	}
       	}
-      	return false; 	
+      	return false; 	/* Return False for failed to send */
     }
-
-    private static Path getFilePath(Path directory, String fileName) throws IOException {
-
-        Path filePath = directory.resolve(fileName);
-        if (Files.exists(filePath)) {
-            return filePath;
-        } else {
-            throw new IOException("Could not resolve the file path.");
-        }
-    }
-
-    private static byte[] getFileBytes(Path filePath) throws IOException {
-
-        byte[] fileBytes = Files.readAllBytes(filePath);
-        if (fileBytes.length < Tftp.LIMIT) {
-            return fileBytes;
-        } else {
-            throw new IOException("Could not get file bytes from path.");
-        }
-    }
-
 }
